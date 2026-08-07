fb5372aae69867bdba474fe834eee7860900fbfa
Juan Gouveia <leo.2k5@gmail.com>
2026-08-07 18:27:47 +0000

    Añadidas dos matrices

---
+++ src/cinema.ts
@@ -0,0 +1,23 @@
+let matrizInicial: number [][] = [
+    [X, A, B, C, D, E, F, G, H, I, J],
+    [1, L, L, L, L, L, L, L, L, L, L],
+    [2, L, L, L, L, L, L, L, L, L, L],
+    [3, L, L, L, L, L, L, L, L, L, L],
+    [4, L, L, L, L, L, L, L, L, L, L],
+    [5, L, L, L, L, L, L, L, L, L, L],
+    [6, L, L, L, L, L, L, L, L, L, L],
+    [7, L, L, L, L, L, L, L, L, L, L],
+    [8, L, L, L, L, L, L, L, L, L, L],
+]
+
+let matrizFinal: number [][] = [
+    [X, A, B, C, D, E, F, G, H, I, J],
+    [1, L, L, L, L, L, L, L, L, L, L],
+    [2, L, L, L, L, L, L, L, L, L, L],
+    [3, L, L, L, L, L, L, L, L, L, L],
+    [4, L, L, L, L, L, L, L, L, L, L],
+    [5, L, L, L, L, L, L, L, L, L, L],
+    [6, L, L, L, L, L, L, L, L, L, L],
+    [7, L, L, L, L, L, L, L, L, L, L],
+    [8, L, L, L, L, L, L, L, L, L, L],
+]

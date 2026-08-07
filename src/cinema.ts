7e59b2e68cc76091b5dbc4739335bfc9399a72dd
Juan Gouveia <leo.2k5@gmail.com>
2026-08-07 18:42:49 +0000

    Hechos cambios a las matrices

--- src/cinema.ts
+++ src/cinema.ts
@@ -1,23 +1,24 @@
-let matrizInicial: number [][] = [
-    [X, A, B, C, D, E, F, G, H, I, J],
-    [1, L, L, L, L, L, L, L, L, L, L],
-    [2, L, L, L, L, L, L, L, L, L, L],
-    [3, L, L, L, L, L, L, L, L, L, L],
-    [4, L, L, L, L, L, L, L, L, L, L],
-    [5, L, L, L, L, L, L, L, L, L, L],
-    [6, L, L, L, L, L, L, L, L, L, L],
-    [7, L, L, L, L, L, L, L, L, L, L],
-    [8, L, L, L, L, L, L, L, L, L, L],
+let matrizInicial: string [][] = [
+    ["PS", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["1", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["2", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["3", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["4", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["5", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["6", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["7", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["8", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
 ]
 
-let matrizFinal: number [][] = [
-    [X, A, B, C, D, E, F, G, H, I, J],
-    [1, L, L, L, L, L, L, L, L, L, L],
-    [2, L, L, L, L, L, L, L, L, L, L],
-    [3, L, L, L, L, L, L, L, L, L, L],
-    [4, L, L, L, L, L, L, L, L, L, L],
-    [5, L, L, L, L, L, L, L, L, L, L],
-    [6, L, L, L, L, L, L, L, L, L, L],
-    [7, L, L, L, L, L, L, L, L, L, L],
-    [8, L, L, L, L, L, L, L, L, L, L],
+let matrizFinal: string [][] = [
+    ["PS", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    ["1", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["2", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["3", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["4", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["5", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["6", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["7", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
+    ["8", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"],
 ]
+

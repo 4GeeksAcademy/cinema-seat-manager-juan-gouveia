58742b682180a367f7d75e06cdfb98d9b588e9e5
Juan Gouveia <leo.2k5@gmail.com>
2026-08-12 17:00:59 +0000

    Pruebas para el selector de asientos

---
+++ src/cinema.js
@@ -0,0 +1,21 @@
+"use strict";
+let theaterSeats = [];
+const seatLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+const rows = 8;
+const seatsPerRow = 10;
+// Initialize the matrix with "X" where X indicates an empty seat
+for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < seatsPerRow; j++) {
+        row.push("X");
+    }
+    theaterSeats.push(row);
+}
+function printSeating(matrix) {
+    console.log("Movie Theater Seating Chart:");
+    console.log("   " + seatLetter.slice(0, seatsPerRow).join(" | "));
+    for (let i = 0; i < matrix.length; i++) {
+        console.log(`${i}: ` + matrix[i].join(" | "));
+    }
+}
+printSeating(theaterSeats);
--- src/cinema.ts
+++ src/cinema.ts
@@ -1,21 +1,23 @@
 
 let theaterSeats: string[][] = [];
+const seatLetter: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 const rows = 8;
 const seatsPerRow = 10;
 
-// Initialize the matrix with "Empty" seats
+// Initialize the matrix with "X" where X indicates an empty seat
 for (let i = 0; i < rows; i++) {
   const row: string[] = [];
   for (let j = 0; j < seatsPerRow; j++) {
-    row.push("Empty");
+    row.push("X");
   }
   theaterSeats.push(row);
 }
 
 function printSeating(matrix: string[][]): void {
   console.log("Movie Theater Seating Chart:");
+  console.log("   " + seatLetter.slice(0, seatsPerRow).join(" | "));
   for (let i = 0; i < matrix.length; i++) {
-    console.log(`Row ${i}: ` + matrix[i].join(" | "));
+    console.log(`${i}: ` + matrix[i].join(" | "));
   }
 }
 
